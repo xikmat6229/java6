0cb4fd609515dfe6e871e4ccfcfefce2d74ac414
xikmatilla6229 <xikmatilla996@gmail.com>
2024-03-15 15:57:00 +0500

    java6

---
+++ scrpt.js
@@ -0,0 +1,30 @@
+let ozim =+prompt('0:tosh, 1:qayshi, 2:qog`oz ')
+let comp = Math.round(Math.random()*2)
+console.log(comp);
+ if ( ozim ===0 && comp===0){
+    alert('durang')
+ }
+ else if ( ozim===1 && comp === 0){
+    alert('comp yutti'
+)
+ }
+ else if ( ozim ===0 && comp===1){
+    alert('siz yutdingiz')
+ }
+ else if (ozim ===1 && comp === 2){
+    alert('comp yutdi')
+    
+ }
+ else if (ozim=== 2 && comp === 2){
+    alert('durrang')
+ }
+ else if (ozim ===2 && comp === 0){
+    alert('siz yutdingiz')
+ }
+ else if ( ozim ===1 && comp ===1){
+    alert('durrang')
+
+}
+else if (ozim ===2 && comp ===1){
+    alert('siz yutqazdingiz')
+}
